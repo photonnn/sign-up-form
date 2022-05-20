6b3085bbb077095f2e95307cb39e3db383fe0608
photonnn <wasjiri@gmail.com>
2022-05-20 19:15:45 +0200

    Bug fix(note) + design changes
    Rewrite password checking script, add shadow to
    input when hovered and change style of button.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const pass1 = document.getElementById("password");
 const pass2 = document.getElementById("confirm_pass");
+const pass = [pass1, pass2];
 
 const input_error_text = document.getElementById("input_error");
-const btn = document.getElementsByTagName("button");
 
-btn[0].addEventListener("input", () => {
-    if (pass1.value !== pass2.value) {
-        pass1.classList.add("invalid");
-        pass2.classList.add("invalid");   
-        input_error_text.textContent = "* Passwords do not match!"     
-    } else {
-        pass1.classList.remove("invalid");
-        pass2.classList.remove("invalid");
-        input_error_text.textContent = "";
-        console.log("UWU");
-    }
+pass.forEach(element => {
+    element.addEventListener("input", ()=> {
+        if (pass1.value !== pass2.value) {
+            pass1.classList.add("invalid"); // makes the border red
+            pass2.classList.add("invalid");
+            input_error_text.textContent = "* Passwords do not match!"
+        } else {
+            pass1.classList.remove("invalid");
+            pass2.classList.remove("invalid");
+            input_error_text.textContent = "";
+        }
+    });
 });
