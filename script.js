d0f237b2bc683b28001e4df4ca0d1f8eab64b3af
photonnn <wasjiri@gmail.com>
2022-05-20 14:18:55 +0200

    Add below content and password check, final touch

---
+++ script.js
@@ -0,0 +1,17 @@
+const pass1 = document.getElementById("password");
+const pass2 = document.getElementById("confirm_pass");
+
+const input_error_text = document.getElementById("input_error");
+
+document.addEventListener("input", () => {
+    if (pass1.value !== pass2.value) {
+        pass1.classList.add("invalid");
+        pass2.classList.add("invalid");   
+        input_error_text.textContent = "* Passwords do not match!"     
+    } else {
+        pass1.classList.remove("invalid");
+        pass2.classList.remove("invalid");
+        input_error_text.textContent = "";
+        console.log("UWU");
+    }
+});
